7b7dbba93e3aac902ecb65571f0f4f482755e6fa
jun kobayashi <junkobayashi1025@gmail.com>
2021-02-28 11:31:13 +0000

    Add .env

--- sample.rb
+++ sample.rb
@@ -1,11 +1,13 @@
-require "slack-notify"
+require 'slack-notify'
 require 'clockwork'
 require 'active_support/time'
+require 'dotenv/load'
+
 
 module Clockwork
-  every(3.minutes, 'client.notify') do
-    client = SlackNotify::Client.new(webhook_url: 'https://hooks.slack.com/services/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')
-    client.notify("Hello There!")
+  every(0.1.minutes, 'client.notify') do
+  client = SlackNotify::Client.new(webhook_url: ENV['WEBHOOK_URL'])
+  client.notify("Hello There!")
   end
 end
 
